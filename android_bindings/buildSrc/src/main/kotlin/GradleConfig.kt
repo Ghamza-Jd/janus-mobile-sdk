e586e6ce6277a85595b8b9c2b7c66ac12becd40b
Hamza Jadid <haljadid@proximie.com>
2024-01-07 01:43:09 +0200

    chore: lib boilerplate cleanup

---
+++ android_bindings/buildSrc/src/main/kotlin/GradleConfig.kt
@@ -0,0 +1,11 @@
+object GradleConfigs {
+    const val compileSdk = 34
+    const val targetSdk = 34
+    const val minSdk = 24
+    const val majorVersion = 0
+    const val minorVersion = 1
+    const val patchVersion = 0
+    const val versionName = "$majorVersion.$minorVersion.$patchVersion"
+    const val versionCode = 1
+    const val ndkVersion = "25.0.8775105"
+}
--- android_bindings/jarust/build.gradle.kts
+++ android_bindings/jarust/build.gradle.kts
@@ -9,8 +9,6 @@ android {
 
     defaultConfig {
         minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
 
@@ -30,11 +28,5 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
