96f479b5058964f3baf7cb67a330ded403fcedf2
Hamza Jadid <haljadid@proximie.com>
2024-01-17 12:23:42 +0200

    feat: added the rust definitions

---
+++ src/jaconfig.rs
@@ -0,0 +1,5 @@
+pub struct JaConfig {
+    pub uri: String,
+    pub apisecret: Option<String>,
+    pub root_namespace: Option<String>,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
+mod jacallback;
+mod jaconfig;
+mod jaconnection;
 mod logger;
 
+use crate::jacallback::JaCallback;
+use crate::jaconfig::JaConfig;
+use crate::jaconnection::JaConnection;
 use crate::logger::init_logger;
 
 uniffi::include_scaffolding!("jarust");
