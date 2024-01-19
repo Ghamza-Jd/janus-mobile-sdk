211b799d467ce5a535786d29d3dbcade1c8cd7b2
Hamza Jadid <haljadid@proximie.com>
2024-01-20 01:47:02 +0200

    feat: added connect functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 mod jacallback;
 mod jaconfig;
 mod jaconnection;
+mod jaerror;
 mod logger;
 
 use crate::jacallback::JaCallback;
 use crate::jaconfig::JaConfig;
 use crate::jaconnection::JaConnection;
+use crate::jaerror::JaError;
 use crate::logger::init_logger;
 
 uniffi::include_scaffolding!("jarust");
