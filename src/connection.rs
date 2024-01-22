517b5b5807dc3a76ab737a41e556373d3142e0a9
Hamza Jadid <haljadid@proximie.com>
2024-01-22 20:10:38 +0200

    feat: added create session api

--- src/connection.rs
+++ src/connection.rs
@@ -1,1 +1,11 @@
+use crate::callback::RawJaConnectionCallback;
+use crate::session::RawJaSession;
+use std::sync::Arc;
+
 pub struct RawJaConnection;
+
+impl RawJaConnection {
+    pub fn create(&self, ka_interval: u32, cb: Box<dyn RawJaConnectionCallback>) {
+        cb.on_session_creation_success(Arc::new(RawJaSession))
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,22 @@ mod connection;
 mod context;
 mod error;
 mod logger;
+mod session;
 
-use crate::callback::RawJaCallback;
+use crate::callback::RawJaConnectionCallback;
 use crate::config::RawJaConfig;
 use crate::connection::RawJaConnection;
 use crate::context::RawJaContext;
 use crate::error::RawJaError;
 use crate::logger::raw_jarust_init_logger;
+use crate::session::RawJaSession;
 use std::sync::Arc;
 
-pub fn raw_jarust_connect(ctx: Arc<RawJaContext>, config: RawJaConfig, cb: Box<dyn RawJaCallback>) {
+pub fn raw_jarust_connect(
+    ctx: Arc<RawJaContext>,
+    config: RawJaConfig,
+    cb: Box<dyn RawJaConnectionCallback>,
+) {
     let root_namespace = config.root_namespace.unwrap_or(String::from("janus"));
     let config = jarust::jaconfig::JaConfig::new(
         &config.uri,
