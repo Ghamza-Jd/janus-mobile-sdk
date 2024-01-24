1a3771bce5a7bd501c6be33fffecd416dca7ef34
Hamza Jadid <haljadid@proximie.com>
2024-01-24 17:47:00 +0200

    feat: update lib + added jaconnection instance to RawJaConnection

--- src/connection.rs
+++ src/connection.rs
@@ -1,11 +1,30 @@
 use crate::callback::RawJaConnectionCallback;
+use crate::context::RawJaContext;
 use crate::session::RawJaSession;
+use jarust::jaconnection::JaConnection;
 use std::sync::Arc;
 
-pub struct RawJaConnection;
+pub struct RawJaConnection {
+    connection: JaConnection,
+}
 
 impl RawJaConnection {
-    pub fn create(&self, ka_interval: u32, cb: Box<dyn RawJaConnectionCallback>) {
-        cb.on_session_creation_success(Arc::new(RawJaSession))
+    pub fn new(connection: JaConnection) -> Self {
+        Self { connection }
+    }
+
+    pub fn create(
+        &self,
+        ctx: Arc<RawJaContext>,
+        ka_interval: u32,
+        cb: Box<dyn RawJaConnectionCallback>,
+    ) {
+        let mut conn = self.connection.clone();
+        ctx.rt.spawn(async move {
+            match conn.create(ka_interval).await {
+                Ok(_) => cb.on_session_creation_success(Arc::new(RawJaSession)),
+                Err(_) => cb.on_session_creation_failure(),
+            }
+        });
     }
 }
