d20958305d99798b03da49bf166167511a644579
Ghamza-Jd <hamzah.jadeed@gmail.com>
2024-09-10 16:26:04 +0300

    feat: added apple logger init

---
+++ apple/Sources/JanusGateway/JanusLogger.swift
@@ -0,0 +1,15 @@
+//
+//  JaLogger.swift
+//
+//
+//  Created by Hamza Jadid on 05/04/2024.
+//
+
+import UniFFI
+
+/// The Logger used in JanusGateway
+public struct JaLogger {
+    public static func initLogger() {
+        rawInitLogger()
+    }
+}
