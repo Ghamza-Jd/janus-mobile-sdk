cf60b858a7adb49c702c85d21e3e77509a0cd636
Ghamza-Jd <hamzah.jadeed@gmail.com>
2024-09-16 15:40:12 +0300

    feat(android): added jahandle

--- android/core/src/main/java/com/ghamza/janus/core/JaSession.kt
+++ android/core/src/main/java/com/ghamza/janus/core/JaSession.kt
@@ -1,8 +1,14 @@
 package com.ghamza.janus.core
 
 import com.ghamza.janus.bindings.Session
+import java.time.Duration
 
 class JaSession(val session: Session) {
     val lower: Session
         get() = session
+
+    suspend fun attach(pluginId: String, timeout: Duration): JaHandle {
+        val handle = session.attach(pluginId = pluginId, timeout = timeout)
+        return JaHandle(handle = handle)
+    }
 }
